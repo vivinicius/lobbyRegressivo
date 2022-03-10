adf567ac3bc3f81e26ea755a5ec8e7718db78199
vivinicius <vi_viniciusferreira@outlook.com>
2022-03-09 21:51:38 -0300

    loading

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import gatlingLogo from './testAleloTransparente.png';
 import ReactTooltip from 'react-tooltip';
 import Collapsible from './Collapsible';
+import ReactLoading from 'react-loading';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
   const [link, setLink] = useState('');
   const [showResults, setShowResult] = useState(false);
   const [buttonSelected, setButtonSelected] = useState('');
@@ -13,7 +15,14 @@ function App() {
     !showResults && setShowResult(true);
     setLink(params);
     setButtonSelected(params);
+    setIsLoading(true);
   };
+
+  useEffect(() => {
+    if (isLoading) {
+      setTimeout(() => setIsLoading(false), 800);
+    }
+  }, [isLoading]);
 
   return (
     <>
@@ -26,19 +35,19 @@ function App() {
       <div class="container">
         <div class="allTest">
           <div class="test1"> 
-            <Collapsible version="X" date="X/X/XXXX" platform="Android" onClick={()=>handleLink('https://vivinicius.github.io/relatoriosRegressivo/relatorio1/')}>
+            <Collapsible version="X" date="X/X/XXXX" platform="Android" onClick={()=>handleLink('https://vivinicius.github.io/relatoriosRegressivo/relatorio2Android/')}>
               <ReactTooltip />
             </Collapsible>
           </div>
           <br />
           <div class="test2">
-            <Collapsible version="X" date="X/X/XXXX" platform="Web" onClick={()=>handleLink('https://vivinicius.github.io/testAlelo/')}>
+            <Collapsible version="X" date="X/X/XXXX" platform="Web" onClick={()=>handleLink('https://vivinicius.github.io/relatoriosRegressivo/relatorio3Web/')}>
               <ReactTooltip />
             </Collapsible>
           </div>
           <br />
           <div class="test2">
-            <Collapsible version="X" date="X/X/XXXX" platform="iOS" onClick={()=>handleLink('https://vivinicius.github.io/testAlelo/')}>
+            <Collapsible version="X" date="X/X/XXXX" platform="iOS" onClick={()=>handleLink('https://vivinicius.github.io/relatoriosRegressivo/relatorio1Android/')}>
               <ReactTooltip />
             </Collapsible>
           </div>
@@ -54,8 +63,9 @@ function App() {
 
 
         {showResults ? (
-          <div class="linkGatling">
-            <iframe src={link} width="40%" height="500px" />
+          <div class="linkGatling">            
+            <iframe className={isLoading && 'isLoading'} src={link} width="40%" height="500px" />
+            {isLoading && <ReactLoading className="loading" color="#8edfa1" type="spin" height={'10%'} width={'10%'} />}
           </div>
         ) : (
           <div class="testAleloTransparente">
